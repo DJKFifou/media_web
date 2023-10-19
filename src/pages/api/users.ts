db52ce04fc62c0778e50b8baaa495ad120fdd2f1
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-19 17:21:43 +0200

    add users api

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,2 +1,48 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "@/lib/prisma";
+import {Role, User} from "@prisma/client";
+
+
+export default async function getUser(req: NextApiRequest, res: NextApiResponse){
+	const data = req.body;
+	const id = data.id
+	console.log(`id : ${id}`)
+	const user = await prisma.test.findMany()
+	console.log(user)
+	return res.status(200).json(user)
+}
+
+
+async function deleteUser(req: NextApiRequest, res: NextApiResponse){
+	const data = req.body;
+	const id = data.id;
+	const deleteUser = await prisma.user.delete({
+		where: {
+			id: id
+		}
+	})
+	return res.json(deleteUser)
+}
+
+async function getAllUsers(req: NextApiRequest, res: NextApiResponse){
+	const allUsers = await prisma.user.findMany()
+	return res.json(allUsers)
+}
+
+async function createUser(req: NextApiRequest, res: NextApiResponse){
+	const data: User = req.body;
+	const {user_name, email, password, article_number, article_frequency, format, theme} = data;
+	const newUser = await prisma.user.create({
+		data: {
+			user_name: user_name,
+			email: email,
+			password: password,
+			type: Role.USER,
+		}
+	})
+}
+
+
+
+
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,22 @@ import styles from '@/styles/Home.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
+async function getUser(){
+  try {
+  const data = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({id: 'clnwy79320001djrbnb08txod'}),
+  })
+  const user = await data.json()
+  console.log(user)
+  }catch (e) {
+    console.error(e)
+  }
+}
+
 export default function Home() {
   return (
     <>
@@ -29,6 +45,7 @@ export default function Home() {
             <div className={`${styles.contentConnexion} ${styles.connexionLinks}`}>
               <a href="/register" className={styles.inscriptionLink}>S'inscrire</a>
               <button>Se connecter</button>
+              <button onClick={() => getUser()}>getUser</button>
             </div>
           </div>
         </div>
