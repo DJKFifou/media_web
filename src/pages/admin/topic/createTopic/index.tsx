b27034c7d60901cfd6913bd43572363bcb0476f3
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-24 18:39:54 +0200

    CRUD Topics

--- src/pages/admin/topic/createTopic/index.tsx
+++ src/pages/admin/topic/createTopic/index.tsx
@@ -2,15 +2,27 @@ import Link from "next/link";
 import {FormEvent, useEffect, useState} from "react";
 import {Article, Theme, Topic} from "@prisma/client";
 import useTheme from "@/hooks/useTheme";
+import useTopic from "@/hooks/useTopic";
+import {useRouter} from "next/router";
 
 export default function CreateTopic(){
-	const {getThemes} = useTheme()
-	const [newTopic, setNewTopic] = useState<Topic | null>(null);
+	const {getThemes} = useTheme();
+	const {createTopic} = useTopic();
+	const router = useRouter()
 	const [themesList, setThemesList] = useState<Theme[] | null>(null);
 	const [articlesList, setArticlesList] = useState<Article[] | null>(null);
 
 	function onSubmit(event: FormEvent<HTMLFormElement>){
-		console.log(event)
+		event.preventDefault()
+		const newTopic: Topic = {
+			title: event.currentTarget.title.value,
+			introduction_text: event.currentTarget.introduction_text.value,
+			theme: event.currentTarget.theme.value,
+			is_hot: event.currentTarget.is_hot.checked,
+		}
+		createTopic(newTopic).then(() => {
+			router.push('/admin')
+		})
 	}
 
 	useEffect(() => {
@@ -23,24 +35,25 @@ export default function CreateTopic(){
 				<h1>Votre nouveau sujet</h1>
 				<form onSubmit={onSubmit}>
 					<label>Titre</label>
-					<input type="text" name="title"/>
+					<input type="text" name="title" id="title"/>
 					<label>Résumé</label>
-					<input type="text" name="introduction_text"/>
+					<input type="text" name="introduction_text" id="introduction_text"/>
 					<label>Themes</label>
 					{themesList && themesList?.length > 0 ? (
-							<select name="theme">
+							<select name="theme" id="theme">
 								{themesList.map((theme, index) => {
 									return (
-											<option value={theme.title} key={index}>{theme.slug}</option>
+											<option value={theme.id} key={index}>{theme.slug}</option>
 									)
 								})}
 							</select>
 					) : null}
 					<label>Sujet hot ?</label>
-					<input type="checkbox"/>
+					<input type="checkbox" name="is_hot" id="is_hot"/>
 					<label>Articles associé</label>
+					{/*//TODO: get article per theme*/}
 					<label>Piste audio</label>
-					<input type="file"/>
+					<input type="file" name="audio" id="audio"/>
 					<button type="submit">Ajouter le sujet</button>
 				</form>
 			</div>
