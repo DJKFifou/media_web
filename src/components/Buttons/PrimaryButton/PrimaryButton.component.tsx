562e70e415a9bcd724aabe2cdae14dae66e6a7d4
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2023-10-25 00:58:48 +0200

    login & register pages

--- src/components/Buttons/PrimaryButton/PrimaryButton.component.tsx
+++ src/components/Buttons/PrimaryButton/PrimaryButton.component.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 const PrimaryButton = (props: any) => {
         return (
-                <button className={styles.primaryButton}>
+                <button type={props.type} className={styles.primaryButton}>
                         {props.title}
                 </button>
         );
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,19 +1,14 @@
-import { FormEvent, useEffect, useState } from 'react'
-import Link from 'next/link'
+import { FormEvent, useState } from 'react'
 import { Inter } from 'next/font/google'
 import { Credentials } from "@/types";
 import useAuth from "@/hooks/useAuth";
 import { useRouter } from "next/router";
 import { supabase } from "@/lib/initSupabase";
 import styles from '@/styles/Home.module.scss'
-import { Article_Frequency, Format, Theme } from '@prisma/client'
+import { Article_Frequency } from '@prisma/client'
 import PrimaryButton from '@/components/Buttons/PrimaryButton/PrimaryButton.component'
-import SecondaryButton from '@/components/Buttons/SecondaryButton/SecondaryButton.component'
-import PrimaryCard from '@/components/Cards/PrimaryCard/PrimaryCard.component'
-import SecondaryCard from '@/components/Cards/SecondaryCard/SecondaryCard.component'
-import ThemeCard from '@/components/Cards/ThemeCard/ThemeCard.component';
-import OneButton from '@/components/Buttons/OneButton/OneButton.component';
 import BackButton from '@/components/Buttons/BackButton/BackButton.component';
+import OneButton from '@/components/Buttons/OneButton/OneButton.component';
 import InputButton from '@/components/Buttons/InputButton/InputButton.component';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,8 +18,7 @@ export default function Register() {
   const router = useRouter()
   const [userCredentials, setUserCredentials] = useState<Credentials | null>(null);
   const [pseudo, setPseudo] = useState<string | null>(null)
-  const [themes, setThemes] = useState<Theme[]>([])
-  const [formats, setFormats] = useState<Format[]>([])
+  const [selectedImage, setSelectedImage] = useState('');
   const articleFrequencyList = [
     {
       label: 'jours',
@@ -39,42 +33,15 @@ export default function Register() {
       value: Article_Frequency.MONTH,
     }
   ]
-  const [selectedArticleFrequencies, setSelectedArticleFrequencies] = useState<Article_Frequency | null>(null)
-  const [numberArticle, setNumberArticle] = useState<number | null>(null)
-  const [selectedThemes, setSelectedThemes] = useState([])
-  const [selectedFormats, setSelectedFormats] = useState([])
-  async function createUser() {
-    console.log(pseudo)
-    try {
-      await fetch('/api/users/createUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          user_name: pseudo,
-          theme: selectedThemes,
-          format: selectedFormats,
-          article_number: parseInt(numberArticle, 10),
-          article_frequency: selectedArticleFrequencies
-        }),
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-  async function getThemes() {
-    try {
-      const themes = await fetch('/api/themes', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const themesJSON = await themes.json()
-      setThemes(themesJSON)
-    } catch (e) {
-      console.error(e)
+  const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        setSelectedImage(e.target.result);
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
     }
   }
   async function onSignUp(event: FormEvent<HTMLFormElement>) {
@@ -92,139 +59,62 @@ export default function Register() {
       console.log(e)
     }
   }
-  async function getFormats() {
-    try {
-      const formats = await fetch('/api/formats', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const formatsJSON = await formats.json()
-      setFormats(formatsJSON)
-    } catch (e) {
-      console.error(e)
-    }
-  }
-  function handleChangeTheme(themeSelected) {
-    console.log(themeSelected)
-    if (selectedThemes.includes(themeSelected)) {
-      const updatedSelectedThemes = selectedThemes.filter(theme => theme === themeSelected);
-      setSelectedThemes(updatedSelectedThemes)
-    } else {
-      setSelectedThemes([...selectedThemes, themeSelected])
-    }
-    console.log(selectedThemes)
-  }
-  function handleChangeFormat(formatSelected) {
-    console.log(formatSelected)
-    if (selectedFormats.includes(formatSelected)) {
-      const updatedSelectedFormats = selectedFormats.filter(format => format === formatSelected);
-      setSelectedFormats(updatedSelectedFormats)
-    } else {
-      setSelectedFormats([...selectedFormats, formatSelected])
-    }
-    console.log(selectedFormats)
-  }
-  // function handleChangeFormat() {
-  //   const checkboxesFormats = document.querySelectorAll('.formatCheckbox');
-  //   const selectedFormats = [];
-  //   checkboxesFormats.forEach(checkbox => {
-  //     if (checkbox.checked) {
-  //       const formatId = checkbox.getAttribute('data-format-id');
-  //       const format = formats.find(format => format.id === formatId);
-  //       selectedFormats.push(format.title);
-  //     }
-  //   });
-  //   console.log(selectedFormats);
-  // }
-  useEffect(() => {
-    getFormats();
-    getThemes();
-  }, [])
-
-
-  function handleChange(key: string, value: string) {
-    setUserCredentials(prevState => ({ ...prevState, [key]: value }))
-  }
   return (
     <>
-      <div>
-        <form onSubmit={onSignUp}>
-          <label>Email</label>
-          <input type="text" onChange={(event) => handleChange('email', event.target.value)} />
-          <label>Mot de passe</label>
-          <input type="password" onChange={(event) => handleChange('password', event.target.value)} />
-          <button type="submit">Suivant</button>
-        </form>
-      </div>
       <div className={`${styles.main} ${inter.className}`}>
-        <div className={styles.sectionConnexion}>
-          <h1 className={styles.titleConnexion}>S'inscrire</h1>
-          <div className={styles.containerConnexion}>
-            <form onSubmit={() => createUser()}>
-              <div className={styles.contentConnexion}>
-                <label htmlFor="">Pseudo</label>
-                <input type="text" placeholder='Johnny' onChange={(event) => { setPseudo(event.target.value) }} />
+        <section className={styles.sectionRegister}>
+          <div className={styles.containerBreadcrumb}>
+            <div className={styles.breadcrumb}>
+              <BackButton />
+              <div className={styles.containerProgress}>
+                <div className={styles.contentProgress}></div>
               </div>
-              {/* <div className={styles.contentConnexion}>
-                <label htmlFor="">Email</label>
-                <input type="email" placeholder='john.doe@gmail.com' onChange={(event) => onUpdateUserCredentials('email', event.target.value)}/>
-              </div>
-              <div className={styles.contentConnexion}>
-                <label htmlFor="">Mot de passe</label>
-                <input type="password" placeholder='**************'/>
-              </div> */}
-              <div className={`${styles.contentConnexion} ${styles.flexColumn}`}>
-                {themes.map((theme: Theme, index) => (
-                  <div key={theme.id} className={styles.checkboxThemes}>
-                    <input data-theme-id={theme.id} name={theme.slug} className="themeCheckbox" key={index} type="checkbox" onChange={() => handleChangeTheme(theme.title)} />
-                    <label>{theme.slug}</label>
-                  </div>
-                ))}
-              </div>
-              <div className={styles.contentConnexion}>
-                {formats.map((format: Format, index) => (
-                  <div key={format.id} className={styles.checkboxFormats}>
-                    <input data-format-id={format.id} name={format.slug} className="formatCheckbox" key={index} type="checkbox" onChange={() => handleChangeFormat(format.title)} />
-                    <label>{format.slug}</label>
-                  </div>
-                ))}
-              </div>
-              {/* <div className={styles.contentConnexion}>
-                <select name="format" id="format" className={styles.selection} multiple>
-                  {formats.map((format: Format, index) => (
-                    <option value={format.slug} key={index}>{format.slug}</option>
-                  ))}
-                </select>
-              </div> */}
-              <div className={styles.contentConnexion}>
-                <input id='numberArticle' type="number" min="0" onChange={(event) => { setNumberArticle(event.target.value); console.log(event.target.value) }} />
-                <select name="frequency" id="frequency" className={styles.selection} multiple onChange={(event) => { setSelectedArticleFrequencies(event.target.value); console.log(event.target.value) }} >
-                  {articleFrequencyList.map((frequency) => (
-                    <option key={frequency.value} value={frequency.value}>
-                      {frequency.label}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className={`${styles.contentConnexion} ${styles.connexionLinks}`}>
-                <Link href="/" className={styles.inscriptionLink}>Se connecter</Link>
-                <button type='submit' className={styles.registerButton}>S'inscrire</button>
-              </div>
-              <div className={`${styles.contentConnexion} ${styles.connexionLinks}`}>
-                <PrimaryButton title="Tout afficher (8)" />
-                <SecondaryButton title="Tout afficher (8)" />
-                <PrimaryCard title="Guerre Israélo-Palestinienne" label="International" />
-                <SecondaryCard title="Titre long sur deux lignes" label="Sport" />
-                <ThemeCard img="/assets/geopolitic.svg" alt="Image Géopolitique" label="Géopolitique" />
-                <OneButton title="Le récap en 5 min" img="/assets/play.svg" alt="Image Play" />
-                <BackButton />
-                <InputButton type="text" placeholder="Pseudo" />
-              </div>
-            </form>
+            </div>
           </div>
-        </div>
+          <div className={styles.containerRegister}>
+            <h2 className={styles.titleRegister}>Créer un compte</h2>
+            <div className={styles.contentRegister}>
+              <form onSubmit={onSignUp}>
+                <div className={styles.profilePicture}>
+                  {/* <img src={selectedImage} alt="" />
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    style={{ display: 'none' }}
+                    id="imageInput"
+                  /> */}
+                  <img src="/assets/profilePicture.png" alt="" className={styles.pp} />
+                  <OneButton title="Sélectionner une photo" />
+                  {/* <label htmlFor="imageInput">
+                    <button>Sélectionner une photo</button>
+                  </label> */}
+                </div>
+                <div className={styles.containerInput}>
+                  <div className={styles.containerPseudo}>
+                    <p>Pseudo</p>
+                    <InputButton type="text" placeholder='Pseudo' onChange={(event) => { setPseudo(event.target.value) }} />
+                  </div>
+                  <div className={styles.containerEmail}>
+                    <p>Adresse mail</p>
+                    <InputButton type="email" placeholder='Adresse mail' onChange={(event) => handleChange('email', event.target.value)} />
+                  </div>
+                  <div className={styles.containerPassword}>
+                    <p>Mot de passe</p>
+                    <InputButton type="password" placeholder='Mot de passe' onChange={(event) => handleChange('password', event.target.value)} />
+                  </div>
+                  <div className={styles.containerCheckbox}>
+                    <input type="checkbox" />
+                    <p>Je confirme avoir lu et être en accord avec les <a href="/">conditions générales d’utilisation</a>.</p>
+                  </div>
+                </div>
+                <div className={styles.containerContinue}>
+                  <PrimaryButton type="submit" title="Continuer" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
       </div>
     </>)
 }
