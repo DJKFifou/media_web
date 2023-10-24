105cca8d7194ea4fbb0a27722cab776553966d63
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-10-24 18:04:25 +0200

    [APP] CRUD ARticle

--- src/hooks/useArticle.ts
+++ src/hooks/useArticle.ts
@@ -1,42 +1,46 @@
-import {Article} from "@prisma/client";
+import { Article } from "@prisma/client"
 
-export default function useArticle(){
-	async function getArticle(id: string){
-		try {
-			const response = await fetch(`/api/articles/getArticle?id=${id}`, {
-				method: 'GET',
-				headers: {
-					"Content-Type": "application/json"
-				},
-			})
-			return await response.json();
-		}catch (e) {
-			console.error(e)
-		}
-	}
+export default function useArticle() {
+  async function getArticle(id: string) {
+    try {
+      const response = await fetch(`/api/articles/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      return await response.json()
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-	async function updateArticle(article: Article){
-		try {
-			const response = await fetch('/api/articles/updateArticle', {
-				method: 'POST',
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({title: article.title, content: article.content, reading_duration: article.reading_duration, id: article.id})
-			})
-		}catch (e) {
-			console.error(e)
-		}
-	}
+  async function updateArticle(id: string, article: Pick<Article, "title" | "content" | "reading_duration">) {
+    try {
+      const response = await fetch(`/api/articles/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: article.title,
+          content: article.content,
+          reading_duration: article.reading_duration,
+        }),
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-	async function getArticles(){
-		try {
-			const data = await fetch('/api/articles/getArticles')
-			return await data.json();
-		}catch (e) {
-			console.error(e)
-		}
-	}
+  async function getArticles() {
+    try {
+      const data = await fetch("/api/articles")
+      return await data.json()
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-	return {getArticle, updateArticle, getArticles}
+  return { getArticle, updateArticle, getArticles }
 }
