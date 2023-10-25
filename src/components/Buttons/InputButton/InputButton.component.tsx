b5b2acf0b438d526b8fc709d146ada3b4254d50b
Maxime Lust <94984112+DJKFifou@users.noreply.github.com>
2023-10-25 16:24:47 +0200

    [UI] Create components register (#3)

--- src/components/Buttons/InputButton/InputButton.component.tsx
+++ src/components/Buttons/InputButton/InputButton.component.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 const InputButton = (props: any) => {
 
   return (
-    <input className={styles.inputButton} type={props.type} placeholder={props.placeholder} />
+    <input className={styles.inputButton} type={props.type} placeholder={props.placeholder} onChange={props.onChange} />
   );
 };
 
