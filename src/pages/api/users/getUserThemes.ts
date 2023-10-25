1ac47c297f3bbd605ac05a344518fc1a5bf991db
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-25 16:16:49 +0200

    update user api

---
+++ src/pages/api/users/getUserThemes.ts
@@ -0,0 +1,15 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {prisma} from "@/lib/prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+	const {userId} = req.query;
+	const themes = await prisma.user.findUnique({
+		where: {
+			id: userId as string
+		},
+		include:{
+			subscribed_themes: true
+		},
+	});
+	return res.status(200).json(themes)
+}
