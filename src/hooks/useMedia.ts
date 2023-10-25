9d41ef1e018385a13a711b482bcfa07a8f5b9b69
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-25 11:41:08 +0200

    CRUD Media

---
+++ src/hooks/useMedia.ts
@@ -0,0 +1,8 @@
+export default function useMedia(){
+	async function getMedias(){
+		const response = await fetch('/api/medias/getMedias');
+		return await response.json()
+	}
+
+	return {getMedias}
+}
---
+++ src/pages/api/medias/getMedias.ts
@@ -0,0 +1,7 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {prisma} from "@/lib/prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+	const medias = await prisma.media.findMany();
+	res.status(200).json(medias)
+}
