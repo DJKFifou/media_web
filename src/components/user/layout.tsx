85e5e435a48e234a75627dd36ad9161813863d18
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-25 16:16:19 +0200

    feat: friend page

---
+++ src/components/user/layout.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+type LayoutType = {
+	userId: string,
+	children: React.ReactNode
+}
+
+export default function Layout({userId, children}: LayoutType){
+	return(
+			<div>
+				{children}
+				<Link href={`/users/${userId}/`}>Vos actualité</Link>
+				<Link href={`/users/${userId}/friends/`}>Vos amis</Link>
+			</div>
+	)
+}
