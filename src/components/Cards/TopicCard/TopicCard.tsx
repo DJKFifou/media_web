2213f926561ab960c3e6bc9ecee05244172150b7
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-10-26 09:59:16 +0200

    [UI] User Topics typing

--- src/components/Cards/TopicCard/TopicCard.tsx
+++ src/components/Cards/TopicCard/TopicCard.tsx
@@ -1,23 +1,26 @@
-import { Article, Topic } from "@prisma/client";
-import Link from "next/link";
 import ArticleCard from "@/components/Cards/ArticleCard/ArticleCard";
+import { Article, Prisma, Topic } from "@prisma/client";
 
-export default function TopicCard({ topic }: { topic: Topic }){
+export default function TopicCard({
+  topic,
+}: {
+  topic: Prisma.TopicGetPayload<{ include: { articles: true; theme: true } }>;
+}) {
   return (
     <div>
       <h2>{topic.theme.title}</h2>
       <h3>{topic.title}</h3>
       <p>{topic.introduction_text}</p>
       <p>DERNIER ARTICLES</p>
-      {topic.articles.length > 0 ? (
-        topic.articles.map((article: Article, index: number) => {
-          return (
-            <div key={index} style={{paddingBottom: 20}}>
-              <ArticleCard article={article} />
-            </div>
-          )
-        })
-      ) : null}
+      {topic.articles.length > 0
+        ? topic.articles.map((article: Article, index: number) => {
+            return (
+              <div key={index} style={{ paddingBottom: 20 }}>
+                <ArticleCard article={article} />
+              </div>
+            );
+          })
+        : null}
     </div>
-  )
+  );
 }
--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,29 +1,30 @@
-import { User } from ".prisma/client";
+import TopicCard from "@/components/Cards/TopicCard/TopicCard";
 import Layout from "@/components/user/layout";
+import useTheme from "@/hooks/useTheme";
+import useTopic from "@/hooks/useTopic";
 import useUser from "@/hooks/useUser";
+import { Prisma, User } from "@prisma/client";
 import dayjs from "dayjs";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import {Topic} from "@prisma/client";
-import useTopic from "@/hooks/useTopic";
-import useTheme from "@/hooks/useTheme";
-import TopicCard from "@/components/Cards/TopicCard/TopicCard";
+
+type Topics = Prisma.TopicGetPayload<{ include: { articles: true; theme: true } }>[];
 
 export default function User() {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const [topicsList, setTopicsList] = useState<Topic[] | null>(null);
+  const [topicsList, setTopicsList] = useState<Topics>([]);
   const router = useRouter();
   const id = router.query.id as string;
   const { getUser } = useUser();
-  const {getTopicsByThemes} = useTopic()
-  const {getTheme} = useTheme()
+  const { getTopicsByThemes } = useTopic();
+  const { getTheme } = useTheme();
 
   const title = () => {
-    const article_frequency = currentUser?.article_frequency
+    const article_frequency = currentUser?.article_frequency;
     const today = dayjs(); //TODO: dayjs in french
     const dayString = today.format("dddd");
-    const monthString = today.format("MMMM")
+    const monthString = today.format("MMMM");
     switch (article_frequency) {
       case "DAY":
         return `Selection du ${dayString}`;
@@ -39,6 +40,7 @@ export default function User() {
       setCurrentUser(user);
     });
     getTopicsByThemes(id).then((topics) => setTopicsList(topics));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   return (
@@ -46,15 +48,15 @@ export default function User() {
       <h1>{`Bienvenue ${currentUser?.user_name}`}</h1>
       <p>{title()}</p>
       <Link href={`/users/${id}/parameters`}>Mes Parametres</Link>
-      {topicsList && topicsList.length > 0 ? (
-        topicsList.map( (topic, index) => {
-          return (
-            <div style={{paddingBottom: 20}} key={index}>
-              <TopicCard topic={topic}/>
-            </div>
-          )
-        })
-      ) : null}
+      {topicsList && topicsList.length > 0
+        ? topicsList.map((topic, index) => {
+            return (
+              <div style={{ paddingBottom: 20 }} key={index}>
+                <TopicCard topic={topic} />
+              </div>
+            );
+          })
+        : null}
     </Layout>
   );
 }
