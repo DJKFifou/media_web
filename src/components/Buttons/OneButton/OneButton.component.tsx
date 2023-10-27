5efb036267ac00d0b51d79188a683d48099a9200
Auréliane DOR <aureliane.dor@gmail.com>
2023-10-27 14:11:52 +0200

    add audio

--- src/components/Buttons/OneButton/OneButton.component.tsx
+++ src/components/Buttons/OneButton/OneButton.component.tsx
@@ -1,18 +1,22 @@
 import { ComponentProps } from "react";
 import styles from "./OneButton.module.scss";
+import Link from "next/link";
 
 const OneButton = (
   props: ComponentProps<"button"> & {
     title: string;
     img: string;
     alt: string;
+    audioLink: string;
   }
 ) => {
   return (
-    <button type="button" className={styles.oneButton} {...props}>
-      <h3>{props.title}</h3>
-      <img src={props.img} alt={props.alt} />
-    </button>
+    <Link href={props.audioLink}>
+      <button type="button" className={styles.oneButton} {...props}>
+        <h3>{props.title}</h3>
+        <img src={props.img} alt={props.alt} />
+      </button>
+    </Link>
   );
 };
 
