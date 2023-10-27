b79ae7b1c724a0053df1d98b02d02ed525324743
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-10-27 14:32:11 +0200

    dsdqs

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,12 +4,14 @@ import Register from "../../components/onBoarding/Register/Register.component";
 import Topics from "../../components/onBoarding/Topics/Topics.component";
 import Frequency from "../../components/onBoarding/Frequency/Frequency.component";
 import Medias from "../../components/onBoarding/Medias/Medias.component";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RegisterPage() {
   type Step = "step1" | "step2" | "step3" | "step4" | "step5";
-  const [currentStep, setCurrentStep] = useState<Step>("step1");
+  const [currentStep, setCurrentStep] = useState<Step>("step4");
+  const router = useRouter();
   return (
     <>
       <div className={`${inter.className}`}>
@@ -17,7 +19,7 @@ export default function RegisterPage() {
         {currentStep == "step2" ? <Topics onSuccess={() => setCurrentStep("step3")} /> : null}
         {/* {currentStep == "step2" ? <Topics onBack={() => setCurrentStep("step1")} /> : null} */}
         {currentStep == "step3" ? <Frequency onSuccess={() => setCurrentStep("step4")} /> : null}
-        {currentStep == "step4" ? <Medias onSuccess={() => setCurrentStep("step5")} /> : null}
+        {currentStep == "step4" ? <Medias onSuccess={() => router.replace("/")} /> : null}
       </div>
     </>
   );
