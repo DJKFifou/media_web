a12da76a7167fbd132972006f0c50dc9cb610740
Maxime <lust.maxime@outlook.fr>
2023-10-27 09:39:46 +0200

    update home page

--- src/components/Buttons/OneButton/OneButton.component.tsx
+++ src/components/Buttons/OneButton/OneButton.component.tsx
@@ -4,11 +4,14 @@ import styles from "./OneButton.module.scss";
 const OneButton = (
   props: ComponentProps<"button"> & {
     title: string;
+    img: string;
+    alt: string;
   }
 ) => {
   return (
     <button className={styles.oneButton} {...props}>
       <h3>{props.title}</h3>
+      <img src={props.img} alt={props.alt} />
     </button>
   );
 };
--- src/components/Cards/ArticleCard/ArticleCard.tsx
+++ src/components/Cards/ArticleCard/ArticleCard.tsx
@@ -1,15 +1,41 @@
 import { Article } from "@prisma/client";
 import Link from "next/link";
+import styles from "./ArticleCard.module.scss";
 
 export default function ArticleCard({article} : {article: Article}){
+  let durationArticle = "2";
+  let numberReco = "9";
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
-      {article.image ? (
+    <div className={styles.articleCard}>
+      {/* {article.image ? (
         //TODO: import article image's into BDD
         <image href={article.image} />
-      ) : null}
-      <Link href={article.link}>{article.title}</Link>
-      <button>{'Enregistrer l\'article'}</button>
+      ) : null} */}
+      <img className={styles.imageArticle} src={article.image} />
+      <Link className={styles.contentText} href={article.link}>
+        <p>{article.title}</p>
+        <label>{article.label}</label>
+      </Link>
+      <div className={styles.containerButton}>
+        {/* <button className={styles.buttonLike}>
+          <img className={styles.containerLike} src="/assets/containerLike.svg" alt="" />
+          <img className={styles.like} src="/assets/emptyLike.svg" alt="" />
+        </button> */}
+        <button className={styles.buttonReco}>
+          <img className={styles.containerReco} src="/assets/containerReco.svg" alt="" />
+          <img className={styles.reco} src="/assets/emptyReco.svg" alt="" />
+        </button>
+      </div>
+      <div className={styles.infosArticle}>
+        <div className={styles.leftContent}>
+          <img src="/assets/durationArticle.svg" alt="" />
+          <label>{durationArticle} min</label>
+        </div>
+        <div className={styles.rightContent}>
+          <img src="/assets/numberReco.svg" alt="" />
+          <label>{numberReco} recommandations</label>
+        </div>
+      </div>
     </div>
   )
 }
