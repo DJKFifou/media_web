408aff4047cba5ed383fde16603ef6b7493a7a3d
Auréliane DOR <aureliane.dor@gmail.com>
2023-11-21 21:09:04 +0100

    add prettier

--- src/components/user/userName/UserName..tsx
+++
@@ -1,8 +0,0 @@
-import styles from '@/components/user/userName/UserName.module.scss'
-export default function UserName({userName}: {userName: string}){
-  return(
-    <div className={styles.container}>
-      <p className={styles.text}>{userName}</p>
-    </div>
-  )
-}
